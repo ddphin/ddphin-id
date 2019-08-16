548d29521c418963ee5887617d07c05ef869adfa
wuyao.china <wuyao.china@gmail.com>
2019-08-16 11:23:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# 分布式ID 通用安全框架
+usage and details cilck [here(ddphin-id-spring-boot)](https://github.com/ddphin/ddphin-id-spring-boot)
---
+++ src/main/java/com/ddphin/id/environment/NodeEnvironment.java
@@ -0,0 +1,15 @@
+package com.ddphin.id.environment;
+
+import lombok.Data;
+
+/**
+ * NodeEnvironment
+ *
+ * @Date 2019/8/11 下午3:30
+ * @Author ddphin
+ */
+@Data
+public class NodeEnvironment {
+    private Integer dataCenterId;
+    private Integer workerId;
+}
